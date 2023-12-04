b726a71760dda905742a65ed2ff19b410c4287d1
jaime <jaimevaldes71@gmail.com>
2023-12-04 11:58:20 +0100

    NavBar

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,12 +1,21 @@
-namespace proyecto.Models;
-
-public class Cliente
+namespace proyecto.Models
 {
-    public int Id {get;set;}
-    public string Name {get;set;}
-    public int Documento {get;set;}
-    public string Genero {get;set;}
-    public string Domicilio {get;set;}
-    public string Mail {get;set;}
-    public int Telefono {get;set;}
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Documento { get; set; }
+        public string Genero { get; set; }
+        public string Domicilio { get; set; }
+        public string Mail { get; set; }
+        public int? Telefono { get; set; }
+
+        public Cliente()
+        {
+            Name = string.Empty; // o cualquier otro valor predeterminado
+            Genero = string.Empty; // o cualquier otro valor predeterminado
+            Domicilio = string.Empty; // o cualquier otro valor predeterminado
+            Mail = string.Empty; // o cualquier otro valor predeterminado
+        }
+    }
 }
